63d74a8717a75f861bf9c9f146f3a471339867f5
Diego Antonio Hurtado Pimentel <diego-antonio.hurtado-pimentel@hpe.com>
2019-09-03 11:47:34 -0600

    chg: dev: Update interface

--- lib/opentelemetry_python_autoagent_flask/plugin/flask.py
+++ lib/opentelemetry_python_autoagent_flask/plugin/flask.py
@@ -1,11 +1,30 @@
+from importlib.util import spec_from_file_location, module_from_spec
+
 from opentelemetry_python_autoagent.plugin.base import BasePlugin
+
+from flask_opentracing import FlaskTracing
 
 
 class FlaskPlugin(BasePlugin):
 
-    def get_options(self):
+    def run(self, path, tracer):
+        """
+        Runs the file defined in path
+        """
 
-        return {}
+        spec = spec_from_file_location('flask.app', path)
+        module = module_from_spec(spec)
+
+        from ipdb import set_trace
+        set_trace()
+
+        spec.loader.exec_module(module)
+
+        FlaskTracing(tracer, True, module.app)
+
+        module.__name__ = '__main__'
+
+        spec.loader.exec_module(module)
 
 
 __all__ = ['FlaskPlugin']
